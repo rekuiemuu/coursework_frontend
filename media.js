b058cfb84eb945051ceef17d31ecd7b3f4c5b587
rekuiemuu <ayakakparovoziktomas@gmail.com>
2024-12-22 21:52:20 +0300

    update
    update

--- media.js
+++ media.js
@@ -51,9 +51,3 @@ function showMedia(mediaPath, mediaType) {
     }
 }
 
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    const hamburger = document.querySelector('.hamburger-menu');
-    menu.classList.toggle('active');
-    hamburger.classList.toggle('active');
-}
