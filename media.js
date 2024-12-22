c2a2924ece1da1e4bad42b39a7791f7e2ca2bc2a
rekuiemuu <ayakakparovoziktomas@gmail.com>
2024-12-22 18:13:53 +0300

    fix media
    fix media

--- media.js
+++ media.js
@@ -50,3 +50,10 @@ function showMedia(mediaPath, mediaType) {
         mediaContainer.appendChild(videoWrapper);
     }
 }
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const hamburger = document.querySelector('.hamburger-menu');
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
